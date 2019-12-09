715c355795a8b434dabb582e4b36ce0789132098
Lechinka1488 <57279252+Lechinka1488@users.noreply.github.com>
2019-12-09 18:52:45 +0500

    Add files via upload
    Смольников залил тест

---
+++ TestSmolnikov.java
@@ -0,0 +1,21 @@
+package startsev;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import startsev.Calculate;
+
+public class TestSmolnikov extends Calculate{
+
+	@Test
+	public void test() {
+		Calculate w = new Calculate();
+	      w.timeThread.start();
+		first=57;
+		second=171;
+		double num=first/(second*second)*10000;
+		if (first<=0) {fail ();}
+		if (second<=0) {fail ();}
+		if (num<=0) {fail ();}
+		if (num!=19.49317738791423) {fail ();}
+	} 
+}
