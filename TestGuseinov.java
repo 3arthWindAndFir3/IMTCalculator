97849d44368cfbd1948a73b6eada75eceebb6372
HennesseyTXR <58680667+HennesseyTXR@users.noreply.github.com>
2019-12-09 11:23:47 +0300

    Create TestGuseinov.java

---
+++ TestGuseinov.java
@@ -0,0 +1,22 @@
+package startsev;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import startsev.Calculate;
+import startsev.Actions;
+
+
+public class TestGuseinov extends Calculate{
+
+	@Test
+	public void test() {
+		Calculate w = new Calculate();
+	      w.timeThread.start();
+		first=58;
+		second=180;
+		double num=first/(second*second)*10000;
+		if (first==0) {fail ();}
+		if (second==0) {fail ();}
+		if (num<=0) {fail ();}
+	} 
+}
