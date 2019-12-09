6ac86160339ab29819f6e19cf01b1e04fc27d82d
3arthWindAndFir3 <57279249+3arthWindAndFir3@users.noreply.github.com>
2019-12-09 14:33:57 +0500

    Update TestKhairullin.java

--- TestKhairullin.java
+++ TestKhairullin.java
@@ -13,8 +13,8 @@ public class TestKhairullin extends Calculate{
 		first=49;
 		second=169;
 		double num=first/(second*second)*10000;
-		if (first==0) {fail ();}
-		if (second==0) {fail ();}
+		if (first<=0) {fail ();}
+		if (second<=0) {fail ();}
 		if (num<=0) {fail ();}
 		if (num!=17.15626203564301) {fail ();}
 	} 
