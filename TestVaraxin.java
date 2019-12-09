84163556b1060e503d71dbc6b8529e9da9c34e37
3arthWindAndFir3 <57279249+3arthWindAndFir3@users.noreply.github.com>
2019-12-09 13:49:35 +0500

    Update TestVaraxin.java
    переделал тест для Вараксина

--- TestVaraxin.java
+++ TestVaraxin.java
@@ -3,23 +3,19 @@ package startsev;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import startsev.Calculate;
-import startsev.Actions;
 
-/**
- * @author homepc
- *
- */
 public class TestVaraxin extends Calculate{
 
 	@Test
 	public void test() {
 		Calculate w = new Calculate();
-	      w.timeThread.start();
+	    w.timeThread.start();
 		first=118;
 		second=187;
 		double num=first/(second*second)*10000;
 		if (first==0) {fail ();}
 		if (second==0) {fail ();}
 		if (num<=0) {fail ();}
+		if (num !=33.74417341073522) {fail ();};
 	} 
 }
